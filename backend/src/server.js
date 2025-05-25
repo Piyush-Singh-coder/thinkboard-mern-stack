b8d3e6a7ca7bfdb41bba4b1d9cab27d5671de6ba
Piyush Singh <piyush@gmail.com>
2025-05-25 18:10:55 +0530

    prepared for development

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,23 @@ import {connectDB} from "./config/db.js"
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
+
 dotenv.config(); 
 
 const app = express(); 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+console.log(__dirname)
 
 //middleware - The work done before sending response back. 
-app.use(cors({
+
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173",
 }));
+}
+
 app.use(express.json());  // To paras the req.body 
 app.use(rateLimiter);
 
@@ -20,7 +28,20 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
 
-connectDB().then(() =>{
+if (process.env.NODE_ENV === "production"){
+    
+    if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get(/.*/  , (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+})
+}
+}
+
+
+
+
+connectDB().then(() =>{ 
     app.listen(PORT, ()=>{
     console.log("app is listening to PORT:",PORT);
 })
